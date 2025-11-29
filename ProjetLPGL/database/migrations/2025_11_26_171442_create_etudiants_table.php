2904651a69d5838ea7f7cc8d3d0be3efd81f50d2
OKOMBI Fortune <okombifortune97@gmail.com>
2025-11-29 12:48:03 +0100

    Ajout de mon projet ProjetLPGL

---
+++ ProjetLPGL/database/migrations/2025_11_26_171442_create_etudiants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email');
+            $table->string('date_naissance');
+            $table->string('tel');
+            $table->foreignId('classe_id')->constrained('classes')->onDelete('cascade');
+            $table->string('adresse');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
